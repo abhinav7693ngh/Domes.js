61c8c30edee26eccac4684afb996ccfdf54078bc
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-01 14:48:36 +0530

    Min Binary Heap

--- Domes.js
+++ Domes.js
@@ -2667,7 +2667,7 @@ class BinarySearchTree{
 
 
 
-// ================================ //
+// =================================== //
 
 
 
@@ -2911,26 +2911,288 @@ class MaxBinaryHeap{
 
 
 
-// let my = new MaxBinaryHeap(1,1);
 
-// my.insert(55);
-// my.insert(39);
-// my.insert(41);
-// my.insert(18);
-// my.insert(27);
-// my.insert(12);
-// my.insert(33);
 
-// console.log(my.extractMax());
-// console.log(my.extractMax());
-// console.log(my.extractMax());
-// console.log(my.extractMax());
-// console.log(my.extractMax());
-// console.log(my.extractMax());
-// console.log(my.extractMax());
-// console.log(my.extractMax());
 
-// console.log(my);
+
+
+// =========================================== //
+
+
+
+
+
+
+
+//========== Min Binary Heap ==================//
+
+
+
+
+
+
+/*
+
+=>  Types of Max Binary Heap
+    1. number
+    2. string
+
+=>  Comparison Methods of Binray Search Trees
+    1. number
+        1. Normal comparison (<,>,=)
+    2. string
+        1. Normal ASCII Comparing
+        2. Based on Length
+
+*/
+
+
+const minHeaptypesMap = {
+    1: 'number',
+    2: 'string'
+}
+
+
+const minHeaptypesAndMethods = {
+    1: [1],
+    2: [1, 2]
+}
+
+
+
+
+
+class MinBinaryHeap {
+
+    constructor(type, method) {
+        this.values = [];
+        this.type = type;
+        this.method = method;
+    }
+
+    insert(val) {
+        try {
+            const myCurrentType = minHeaptypesMap[this.type];
+            if (arguments.length > 0) {
+                if (typeof (val) === myCurrentType) {
+                    const supportedMethods = minHeaptypesAndMethods[parseInt(this.type)];
+                    if (supportedMethods.find(ele => parseInt(ele) === parseInt(this.method))) {
+                        this.values.push(val);
+                        let valueIndex = this.values.length - 1;
+                        let parentIndex = Math.floor((valueIndex - 1) / 2);
+                        if (this.type === 1 && this.method === 1) {
+                            while (parentIndex >= 0 && this.values[parentIndex] > this.values[valueIndex]) {
+                                let temp = this.values[parentIndex];
+                                this.values[parentIndex] = this.values[valueIndex];
+                                this.values[valueIndex] = temp;
+                                valueIndex = parentIndex;
+                                parentIndex = Math.floor((valueIndex - 1) / 2);
+                            }
+                            return val;
+                        }
+                        else if (this.type === 2 && this.method === 1) {
+                            while (parentIndex >= 0 && this.values[parentIndex] > this.values[valueIndex]) {
+                                let temp = this.values[parentIndex];
+                                this.values[parentIndex] = this.values[valueIndex];
+                                this.values[valueIndex] = temp;
+                                valueIndex = parentIndex;
+                                parentIndex = Math.floor((valueIndex - 1) / 2);
+                            }
+                            return val;
+                        }
+                        else if (this.type === 2 && this.method === 2) {
+                            while (parentIndex >= 0 && this.values[parentIndex].length > this.values[valueIndex].length) {
+                                let temp = this.values[parentIndex];
+                                this.values[parentIndex] = this.values[valueIndex];
+                                this.values[valueIndex] = temp;
+                                valueIndex = parentIndex;
+                                parentIndex = Math.floor((valueIndex - 1) / 2);
+                            }
+                            return val;
+                        }
+                    }
+                    else {
+                        throw new Error('Method specified is not supported for this selected type please refer documentation for supported methods');
+                    }
+                }
+                else {
+                    throw new Error('Passed value does not match the type of Max Binary Heap');
+                }
+            }
+            else {
+                throw new Error('Argument not passed');
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    extractMin() {
+        try {
+            if (this.values.length !== 0) {
+                if (this.values.length === 1) {
+                    return this.values.pop();
+                }
+                else if (this.values.length === 2) {
+                    return this.values.shift();
+                }
+                else if (this.values.length === 3) {
+                    let max = this.values.shift();
+                    if (this.values[0] > this.values[1]) {
+                        let temp = this.values[1];
+                        this.values[1] = this.values[0];
+                        this.values[0] = temp;
+                    }
+                    return max;
+                }
+                else {
+                    let temp = this.values[0];
+                    this.values[0] = this.values[this.values.length - 1];
+                    this.values[this.values.length - 1] = temp;
+                    const max = this.values.pop();
+                    let index = 0;
+                    let left = this.values[2 * index + 1];
+                    let right = this.values[2 * index + 2];
+                    if (this.type === 1 && this.method === 1) {
+                        while (this.values[index] > left || this.values[index] > right) {
+                            if (right > left) {
+                                let temp2 = this.values[index];
+                                this.values[index] = this.values[2 * index + 1];
+                                this.values[2 * index + 1] = temp2;
+                                index = 2 * index + 1;
+                                left = this.values[2 * index + 1];
+                                right = this.values[2 * index + 2];
+                            }
+                            else {
+                                let temp2 = this.values[index];
+                                this.values[index] = this.values[2 * index + 2];
+                                this.values[2 * index + 2] = temp2;
+                                index = 2 * index + 2;
+                                left = this.values[2 * index + 1];
+                                right = this.values[2 * index + 2];
+                            }
+                        }
+                        return max;
+                    }
+                    else if (this.type === 2 && this.method === 1) {
+                        while (this.values[index] > left || this.values[index] > right) {
+                            if (right > left) {
+                                let temp2 = this.values[index];
+                                this.values[index] = this.values[2 * index + 1];
+                                this.values[2 * index + 1] = temp2;
+                                index = 2 * index + 1;
+                                left = this.values[2 * index + 1];
+                                right = this.values[2 * index + 2];
+                            }
+                            else {
+                                let temp2 = this.values[index];
+                                this.values[index] = this.values[2 * index + 2];
+                                this.values[2 * index + 2] = temp2;
+                                index = 2 * index + 2;
+                                left = this.values[2 * index + 1];
+                                right = this.values[2 * index + 2];
+                            }
+                        }
+                        return max;
+                    }
+                    else if (this.type === 2 && this.method === 2) {
+                        while (this.values[index].length > left.length || this.values[index].length > right.length) {
+                            if (right.length > left.length) {
+                                let temp2 = this.values[index];
+                                this.values[index] = this.values[2 * index + 1];
+                                this.values[2 * index + 1] = temp2;
+                                index = 2 * index + 1;
+                                left = this.values[2 * index + 1];
+                                right = this.values[2 * index + 2];
+                            }
+                            else {
+                                let temp2 = this.values[index];
+                                this.values[index] = this.values[2 * index + 2];
+                                this.values[2 * index + 2] = temp2;
+                                index = 2 * index + 2;
+                                left = this.values[2 * index + 1];
+                                right = this.values[2 * index + 2];
+                            }
+                        }
+                        return max;
+                    }
+                }
+            }
+            else {
+                return null;
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    toArray() {
+        try {
+            return this.values;
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    isEmpty() {
+        try {
+            if (this.values.length === 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    size() {
+        try {
+            return this.values.length;
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+let my = new MinBinaryHeap(1,1);
+
+my.insert(55);
+my.insert(39);
+my.insert(41);
+my.insert(18);
+my.insert(27);
+my.insert(12);
+my.insert(33);
+
+console.log(my.extractMin());
+console.log(my.extractMin());
+console.log(my.extractMin());
+console.log(my.extractMin());
+console.log(my.extractMin());
+console.log(my.extractMin());
+console.log(my.extractMin());
+console.log(my.extractMin());
+
+
+
+
+console.log(my);
 
 
 
