555c36dec176bb69470ec0075b4c15bc7dd4eb44
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-01 18:49:55 +0530

    Priority queues complete

--- Domes.js
+++ Domes.js
@@ -2693,6 +2693,40 @@ class BinarySearchTree{
 
 */
 
+// function maxHeapify(myindex) {
+
+//     let valueIndex = myindex;
+//     let parentIndex = Math.floor((valueIndex - 1) / 2);
+//     if (this.type === 1 && this.method === 1) {
+//         while (parentIndex >= 0 && this.values[parentIndex] < this.values[valueIndex]) {
+//             let temp = this.values[parentIndex];
+//             this.values[parentIndex] = this.values[valueIndex];
+//             this.values[valueIndex] = temp;
+//             valueIndex = parentIndex;
+//             parentIndex = Math.floor((valueIndex - 1) / 2);
+//         }
+//     }
+//     else if (this.type === 2 && this.method === 1) {
+//         while (parentIndex >= 0 && this.values[parentIndex] < this.values[valueIndex]) {
+//             let temp = this.values[parentIndex];
+//             this.values[parentIndex] = this.values[valueIndex];
+//             this.values[valueIndex] = temp;
+//             valueIndex = parentIndex;
+//             parentIndex = Math.floor((valueIndex - 1) / 2);
+//         }
+//     }
+//     else if (this.type === 2 && this.method === 2) {
+//         while (parentIndex >= 0 && this.values[parentIndex].length < this.values[valueIndex].length) {
+//             let temp = this.values[parentIndex];
+//             this.values[parentIndex] = this.values[valueIndex];
+//             this.values[valueIndex] = temp;
+//             valueIndex = parentIndex;
+//             parentIndex = Math.floor((valueIndex - 1) / 2);
+//         }
+//     }
+// }
+
+
 
 const maxHeaptypesMap = {
     1: 'number',
@@ -2977,37 +3011,37 @@ class MaxBinaryHeap{
 
 
 
-function minHeapify(myindex){
-    let valueIndex = myindex;
-    let parentIndex = Math.floor((valueIndex - 1) / 2);
-    if (this.type === 1 && this.method === 1) {
-        while (parentIndex >= 0 && this.values[parentIndex] > this.values[valueIndex]) {
-            let temp = this.values[parentIndex];
-            this.values[parentIndex] = this.values[valueIndex];
-            this.values[valueIndex] = temp;
-            valueIndex = parentIndex;
-            parentIndex = Math.floor((valueIndex - 1) / 2);
-        }
-    }
-    else if (this.type === 2 && this.method === 1) {
-        while (parentIndex >= 0 && this.values[parentIndex] > this.values[valueIndex]) {
-            let temp = this.values[parentIndex];
-            this.values[parentIndex] = this.values[valueIndex];
-            this.values[valueIndex] = temp;
-            valueIndex = parentIndex;
-            parentIndex = Math.floor((valueIndex - 1) / 2);
-        }
-    }
-    else if (this.type === 2 && this.method === 2) {
-        while (parentIndex >= 0 && this.values[parentIndex].length > this.values[valueIndex].length) {
-            let temp = this.values[parentIndex];
-            this.values[parentIndex] = this.values[valueIndex];
-            this.values[valueIndex] = temp;
-            valueIndex = parentIndex;
-            parentIndex = Math.floor((valueIndex - 1) / 2);
-        }
-    }
-}
+// function minHeapify(myindex){
+//     let valueIndex = myindex;
+//     let parentIndex = Math.floor((valueIndex - 1) / 2);
+//     if (this.type === 1 && this.method === 1) {
+//         while (parentIndex >= 0 && this.values[parentIndex] > this.values[valueIndex]) {
+//             let temp = this.values[parentIndex];
+//             this.values[parentIndex] = this.values[valueIndex];
+//             this.values[valueIndex] = temp;
+//             valueIndex = parentIndex;
+//             parentIndex = Math.floor((valueIndex - 1) / 2);
+//         }
+//     }
+//     else if (this.type === 2 && this.method === 1) {
+//         while (parentIndex >= 0 && this.values[parentIndex] > this.values[valueIndex]) {
+//             let temp = this.values[parentIndex];
+//             this.values[parentIndex] = this.values[valueIndex];
+//             this.values[valueIndex] = temp;
+//             valueIndex = parentIndex;
+//             parentIndex = Math.floor((valueIndex - 1) / 2);
+//         }
+//     }
+//     else if (this.type === 2 && this.method === 2) {
+//         while (parentIndex >= 0 && this.values[parentIndex].length > this.values[valueIndex].length) {
+//             let temp = this.values[parentIndex];
+//             this.values[parentIndex] = this.values[valueIndex];
+//             this.values[valueIndex] = temp;
+//             valueIndex = parentIndex;
+//             parentIndex = Math.floor((valueIndex - 1) / 2);
+//         }
+//     }
+// }
 
 
 
@@ -3253,37 +3287,6 @@ class MinBinaryHeap {
             console.log(e);
         }
     }
-
-    updateValue(index,updatedValue){
-        try{
-            const myCurrentType = minHeaptypesMap[this.type];
-            if(arguments.length > 0){
-                if(typeof(index)==='number' && typeof(updatedValue)===myCurrentType){
-                    if(index>=0 && index<this.values.length){
-                        for(let i=0;i<this.values.length;i++){
-                            if(i===index){
-                                this.values[i] = updatedValue;
-                                minHeapify.call(this,index);
-                                return true;
-                            }
-                        }
-                    }
-                    else{
-                        throw new Error('Index out of range');
-                    }
-                }
-                else{
-                    throw new Error('Passed value does not match the type of Max Binary Heap or Index is not of type number');
-                }
-            }
-            else{
-                throw new Error('Argument not passed');
-            }
-        }
-        catch(e){
-            console.log(e);
-        }
-    }
 }
 
 
@@ -3313,17 +3316,17 @@ In section will contain both MinHeap Priority queue and MaxHeap Priority Queue
 */
 
 
-function minHeapifyPriorityQueue(myindex) {
-    let newNodeIndex = myindex;
-    let parentIndex = Math.floor(((newNodeIndex) - 1) / 2);
-    while (parentIndex >= 0 && this.values[parentIndex].priority > this.values[newNodeIndex].priority) {
-        let temp = this.values[parentIndex];
-        this.values[parentIndex] = this.values[newNodeIndex];
-        this.values[newNodeIndex] = temp;
-        newNodeIndex = parentIndex;
-        parentIndex = Math.floor((newNodeIndex - 1) / 2);
-    }
-}
+// function minHeapifyPriorityQueue(myindex) {
+//     let newNodeIndex = myindex;
+//     let parentIndex = Math.floor(((newNodeIndex) - 1) / 2);
+//     while (parentIndex >= 0 && this.values[parentIndex].priority > this.values[newNodeIndex].priority) {
+//         let temp = this.values[parentIndex];
+//         this.values[parentIndex] = this.values[newNodeIndex];
+//         this.values[newNodeIndex] = temp;
+//         newNodeIndex = parentIndex;
+//         parentIndex = Math.floor((newNodeIndex - 1) / 2);
+//     }
+// }
 
 
 
@@ -3339,7 +3342,7 @@ class PriorityQueueNode{
 }
 
 
-class MinHeapPriorityQueue{
+class MinPriorityQueue{
 
     constructor() {
         this.values = [];
@@ -3349,7 +3352,7 @@ class MinHeapPriorityQueue{
         try{
             if(arguments.length > 0){
                 if(val === 0 || val === false || val){
-                    if(typeof(priority) === 'number' && (priority !== Infinity || priority !== -Infinity)){
+                    if(typeof(priority) === 'number' && isFinite(priority)){
                         const newNode = new PriorityQueueNode(val, priority);
                         this.values.push(newNode);
                         let newNodeIndex = (this.values.length) - 1;
@@ -3486,7 +3489,7 @@ class MinHeapPriorityQueue{
         }
     }
 
-    getNodeValue(index){
+    getNodeInfo(index){
         try{
             if(arguments.length > 0){
                 if(typeof(index) === 'number'){
@@ -3494,7 +3497,7 @@ class MinHeapPriorityQueue{
                         if(this.values.length !== 0){
                             for(let i=0;i<this.values.length;i++){
                                 if(i===index){
-                                    return this.values[index].value;
+                                    return {value : this.values[index].value, priority : this.values[index].priority };
                                 }
                             }
                         }
@@ -3526,36 +3529,6 @@ class MinHeapPriorityQueue{
             }
             else{
                 return false;
-            }
-        }
-        catch(e){
-            console.log(e);
-        }
-    }
-
-    updatePriority(index,updatedPriority){
-        try{
-            if(arguments.length > 0){
-                if(typeof(index) === 'number' && typeof(updatedPriority) === 'number'){
-                    if(index>=0 && index < this.values.length){
-                        for(let i=0;i<this.values.length;i++){
-                            if(i === index){
-                                this.values[i].priority = updatedPriority;
-                                minHeapifyPriorityQueue.call(this,index);
-                                return true;
-                            }
-                        }
-                    }
-                    else{
-                        throw new Error('Index out of range');
-                    }
-                }
-                else{
-                    throw new Error('Index or Priority is not of type number');
-                }
-            }
-            else{
-                throw new Error('Arguments not passed');
             }
         }
         catch(e){
@@ -3595,10 +3568,249 @@ class MinHeapPriorityQueue{
 
 
 
-let my =  new MinBinaryHeap(1,1);
-my.insert(10);
-my.insert(9);
 
-console.log(my.updateValue(0,5));
+
+
+
+class MaxPriorityQueue {
+
+    constructor() {
+        this.values = [];
+    }
+
+    enqueue(val, priority) {
+        try {
+            if (arguments.length > 0) {
+                if (val === 0 || val === false || val) {
+                    if (typeof (priority) === 'number' && isFinite(priority)) {
+                        const newNode = new PriorityQueueNode(val, priority);
+                        this.values.push(newNode);
+                        let newNodeIndex = (this.values.length) - 1;
+                        let parentIndex = Math.floor(((newNodeIndex) - 1) / 2);
+                        while (parentIndex >= 0 && this.values[parentIndex].priority < this.values[newNodeIndex].priority) {
+                            let temp = this.values[parentIndex];
+                            this.values[parentIndex] = this.values[newNodeIndex];
+                            this.values[newNodeIndex] = temp;
+                            newNodeIndex = parentIndex;
+                            parentIndex = Math.floor((newNodeIndex - 1) / 2);
+                        }
+                        return val;
+                    }
+                    else {
+                        throw new Error('Priority is not of type number/Infinity/-Infinity');
+                    }
+                }
+                else {
+                    throw new Error('Passed value is undefined/null');
+                }
+            }
+            else {
+                throw new Error('Arguments not passed');
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    dequeue() {
+        try {
+            if (this.values.length !== 0) {
+                if (this.values.length === 1) {
+                    let myValue = this.values.pop();
+                    return myValue.value;
+                }
+                else if (this.values.length === 2) {
+                    let myValue;
+                    if (this.values[0].priority > this.values[1].priority) {
+                        myValue = this.values.shift();
+                    }
+                    else {
+                        myValue = this.values.pop();
+                    }
+                    return myValue.value;
+                }
+                else if (this.values.length === 3) {
+                    let myValue = this.values.shift();
+                    if (this.values[0].priority < this.values[1].priority) {
+                        let temp = this.values[0];
+                        this.values[0] = this.values[1];
+                        this.values[1] = temp;
+                    }
+                    return myValue.value;
+                }
+                else {
+                    let temp = this.values[0];
+                    this.values[0] = this.values[this.values.length - 1];
+                    this.values[this.values.length - 1] = temp;
+                    const myValue = this.values.pop();
+                    let index = 0;
+                    let left = this.values[2 * index + 1];
+                    let right = this.values[2 * index + 2];
+                    while (this.values[index].priority < left.priority || this.values[index].priority < right.priority) {
+                        if (right.priority < left.priority) {
+                            let temp2 = this.values[index];
+                            this.values[index] = this.values[2 * index + 1];
+                            this.values[2 * index + 1] = temp2;
+                            index = 2 * index + 1;
+                            if ((2 * index + 1) > (this.values.length - 1)) {
+                                left = { priority: -Infinity };
+                            }
+                            else {
+                                left = this.values[2 * index + 1];
+                            }
+                            if (2 * index + 2 > (this.values.length - 1)) {
+                                right = { priority: -Infinity };
+                            }
+                            else {
+                                right = this.values[2 * index + 2];
+                            }
+                        }
+                        else {
+                            let temp2 = this.values[index];
+                            this.values[index] = this.values[2 * index + 2];
+                            this.values[2 * index + 2] = temp2;
+                            index = 2 * index + 2;
+                            if ((2 * index + 1) > (this.values.length - 1)) {
+                                left = { priority: -Infinity };
+                            }
+                            else {
+                                left = this.values[2 * index + 1];
+                            }
+                            if (2 * index + 2 > (this.values.length - 1)) {
+                                right = { priority: -Infinity };
+                            }
+                            else {
+                                right = this.values[2 * index + 2];
+                            }
+                        }
+                    }
+                    return myValue.value;
+                }
+            }
+            else {
+                return null;
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    size() {
+        try {
+            return this.values.length;
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    toArray() {
+        try {
+            let toReturn = [];
+            for (let i of this.values) {
+                toReturn.push(i.value);
+            }
+            return toReturn;
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    getNodeInfo(index) {
+        try {
+            if (arguments.length > 0) {
+                if (typeof (index) === 'number') {
+                    if (index >= 0 && index < this.values.length) {
+                        if (this.values.length !== 0) {
+                            for (let i = 0; i < this.values.length; i++) {
+                                if (i === index) {
+                                    return { value: this.values[index].value, priority: this.values[index].priority };
+                                }
+                            }
+                        }
+                        else {
+                            return null;
+                        }
+                    }
+                    else {
+                        throw new Error('Index out of range');
+                    }
+                }
+                else {
+                    throw new Error('Index is not of type number');
+                }
+            }
+            else {
+                throw new Error('Argument not passed');
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    isEmpty() {
+        try {
+            if (this.values.length === 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    updateValue(index, updatedValue) {
+        try {
+            if (arguments.length > 0) {
+                if (typeof (index) === 'number') {
+                    if (index >= 0 && index < this.values.length) {
+                        for (let i = 0; i < this.values.length; i++) {
+                            if (i === index) {
+                                this.values[i].value = updatedValue;
+                                return true;
+                            }
+                        }
+                    }
+                    else {
+                        throw new Error('Index out of range');
+                    }
+                }
+                else {
+                    throw new Error('Index is not of type number');
+                }
+            }
+            else {
+                throw new Error('Arguments not passed');
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+let my =  new MaxPriorityQueue();
+
+my.enqueue(9,10);
+my.enqueue(12,5);
+my.enqueue(13,8);
+my.enqueue(14,10);
+my.enqueue(15,4);
+
 
 console.log(my);
