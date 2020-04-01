307fac4647cddd948a72b96e75920655919b3524
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-01 13:08:24 +0530

    Max Binary Heap scratchy

--- Domes.js
+++ Domes.js
@@ -2675,23 +2675,185 @@ class BinarySearchTree{
 
 
 
-// ======= Binary Heaps =========== //
+// ======= Max Binary Heaps =========== //
+
+
+/*
+
+=>  Types of Max Binary Heap
+    1. number
+    2. string
+
+=>  Comparison Methods of Binray Search Trees
+    1. number
+        1. Normal comparison (<,>,=)
+    2. string
+        1. Normal ASCII Comparing
+        2. Based on Length
+
+*/
+
+
+const maxHeaptypesMap = {
+    1: 'number',
+    2: 'string'
+}
+
+
+const maxHeaptypesAndMethods = {
+    1: [1],
+    2: [1, 2]
+}
 
 
 
 
-let my = new BinarySearchTree(1,1);
-my.insert(30);
-my.insert(10);
-my.insert(50);
-my.insert(5);
-my.insert(20);
-my.insert(35);
-my.insert(60);
-my.insert(15);
-my.insert(40);
 
-console.log(my.getNodeValueWithId(10));
+class MaxBinaryHeap{
+
+    constructor(type,method){
+        this.values = [];
+        this.type = type;
+        this.method = method;
+    }
+
+    insert(val){
+        try{
+            const myCurrentType = maxHeaptypesMap[this.type];
+            if(arguments.length > 0){
+                if(typeof(val) === myCurrentType){
+                    const supportedMethods = maxHeaptypesAndMethods[parseInt(this.type)];
+                    if(supportedMethods.find(ele => parseInt(ele) === parseInt(this.method))){
+                        this.values.push(val);
+                        let valueIndex = this.values.length - 1;
+                        let parentIndex = Math.floor((valueIndex - 1) / 2);
+                        if()
+                        while (parentIndex >= 0 && this.values[parentIndex] < this.values[valueIndex]) {
+                            let temp = this.values[parentIndex];
+                            this.values[parentIndex] = this.values[valueIndex];
+                            this.values[valueIndex] = temp;
+                            valueIndex = parentIndex;
+                            parentIndex = Math.floor((valueIndex - 1) / 2);
+                        }
+                        return val;
+                    }
+                    else{
+                        throw new Error('Method specified is not supported for this selected type please refer documentation for supported methods');
+                    }
+                }
+                else{
+                    throw new Error('Passed value does not match the type of Max Binary Heap');
+                }
+            }
+            else{
+                throw new Error('Argument not passed');
+            }
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    extractMax(){
+        try{
+            if(this.values.length !== 0){
+                if(this.values.length === 1){
+                    return this.values.pop();
+                }
+                else if(this.values.length === 2){
+                    return this.values.shift();
+                }
+                else if(this.values.length === 3){
+                    let max = this.values.shift();
+                    if (this.values[0] < this.values[1]) {
+                        let temp = this.values[1];
+                        this.values[1] = this.values[0];
+                        this.values[0] = temp;
+                    }
+                    return max;
+                }
+                else{
+                    let temp = this.values[0];
+                    this.values[0] = this.values[this.values.length - 1];
+                    this.values[this.values.length - 1] = temp;
+                    const max = this.values.pop();
+                    let index = 0;
+                    let left = this.values[2 * index + 1];
+                    let right = this.values[2 * index + 2];
+                    while (this.values[index] < left || this.values[index] < right) {
+                        if (right < left) {
+                            let temp2 = this.values[index];
+                            this.values[index] = this.values[2 * index + 1];
+                            this.values[2 * index + 1] = temp2;
+                            index = 2 * index + 1;
+                            left = this.values[2 * index + 1];
+                            right = this.values[2 * index + 2];
+                        }
+                        else {
+                            let temp2 = this.values[index];
+                            this.values[index] = this.values[2 * index + 2];
+                            this.values[2 * index + 2] = temp2;
+                            index = 2 * index + 2;
+                            left = this.values[2 * index + 1];
+                            right = this.values[2 * index + 2];
+                        }
+                    }
+                    return max;
+                }
+            }
+            else{
+                return null;
+            }
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    toArray(){
+        try{
+            return this.values;
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    isEmpty(){
+        try{
+            if(this.values.length === 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    size(){
+        try{
+            return this.values.length;
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+}
 
 
-console.log(my);
+
+
+let my = new MaxBinaryHeap();
+
+console.log(my.insert(10));
+
+
+
+
+
+
+
+
